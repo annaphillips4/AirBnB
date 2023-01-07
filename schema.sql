0b9f04d54a5541358004d672de197d4aac2fd562
Anna <annaphillips416@gmail.com>
2023-01-07 17:05:34 -0500

    authenticate-me and schema

---
+++ schema.sql
@@ -0,0 +1,63 @@
+CREATE TABLE `users` (
+  `id` integer PRIMARY KEY,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `email` varchar(255) UNIQUE NOT NULL,
+  `username` varchar(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE `spots` (
+  `id` integer PRIMARY KEY,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255),
+  `state` varchar(255),
+  `country` varchar(255),
+  `lat` decimal(10, 7),
+  `lng` decimal(10, 7),
+  `name` varchar(255),
+  `description` blob,
+  `price` decimal NOT NULL,
+  `avgRating` decimal(2, 1),
+  `previewImage` integer,
+  `ownerId` integer
+);
+
+CREATE TABLE `images` (
+  `id` integer PRIMARY KEY,
+  `url` varchar(255) NOT NULL,
+  `preview` boolean,
+  `spotId` integer
+);
+
+CREATE TABLE `reviews` (
+  `id` integer PRIMARY KEY,
+  `review` blob,
+  `stars` integer NOT NULL,
+  `userId` integer,
+  `spotId` integer,
+  `reviewImages` integer
+);
+
+CREATE TABLE `bookings` (
+  `id` integer PRIMARY KEY,
+  `spotId` integer,
+  `userId` integer,
+  `startDate` datetime NOT NULL,
+  `endDate` datetime NOT NULL
+);
+
+ALTER TABLE `spots` ADD FOREIGN KEY (`previewImage`) REFERENCES `images` (`id`);
+
+ALTER TABLE `spots` ADD FOREIGN KEY (`ownerId`) REFERENCES `users` (`id`);
+
+ALTER TABLE `images` ADD FOREIGN KEY (`spotId`) REFERENCES `spots` (`id`);
+
+ALTER TABLE `reviews` ADD FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+
+ALTER TABLE `reviews` ADD FOREIGN KEY (`spotId`) REFERENCES `spots` (`id`);
+
+ALTER TABLE `reviews` ADD FOREIGN KEY (`reviewImages`) REFERENCES `images` (`id`);
+
+ALTER TABLE `bookings` ADD FOREIGN KEY (`spotId`) REFERENCES `spots` (`id`);
+
+ALTER TABLE `bookings` ADD FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
