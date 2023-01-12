de4d25eba52838753d10bb84c4a5e092a509378f
Anna <annaphillips416@gmail.com>
2023-01-12 18:43:31 -0500

    Add error-handling middleware for common errors and debugging

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
 
-module.exports = router;
-
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
-
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
 })
+
+module.exports = router;
