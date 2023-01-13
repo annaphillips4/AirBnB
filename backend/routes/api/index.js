910b1a769f9dd234cd75f4e7609f6266e53fca04
Anna <annaphillips416@gmail.com>
2023-01-12 19:09:19 -0500

    fixed api

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 router.post('/test', function(req, res) {
+    console.log("testing")
     res.json({ requestBody: req.body });
 })
+
 
 module.exports = router;
