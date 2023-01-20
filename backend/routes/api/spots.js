9e463190556304ecb2709d9bbb2b88a38e7b869c
Anna <annaphillips416@gmail.com>
2023-01-19 20:41:27 -0500

    progressed spot routes

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -11,7 +11,34 @@ const { handleValidationErrors } = require('../../utils/validation');
 // Get all Spots
 router.get('/', async (req, res) => {
     const spots = await Spot.findAll()
-    res.json({spots})
+    return res.json({spots})
+})
+
+// Get details of a Spot from an id
+router.get('/:spotId', async (req, res) => {
+    const spot = await Spot.findByPk(req.params.spotId)
+    // !!!!!!!!! Append images w/ details and owner w/ details !!!!!!!!!!
+    if (spot === null) {
+        const e = new Error("Couldn't find a Spot with the specified id")
+        e.statusCode = 404;
+        e.message = "Spot couldn't be found";
+        return res.json(e)
+    }
+    return res.json({spot})
+})
+
+// Create a spot
+router.post('/', )
+
+// Delete a spot
+router.delete('/:spotId', restoreUser, async (req, res) => {
+    const spot = await Spot.findByPk(req.params.spotId)
+    if (spot === null) {
+        const e = new Error("Couldn't find a Spot with the specified id")
+        e.statusCode = 404;
+        e.message = "Spot couldn't be found";
+        return res.json(e)
+    }
 })
 
 module.exports = router;
