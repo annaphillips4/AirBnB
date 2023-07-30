86077d96d0abe9c777933c26c2d3e426e1362c2a
Anna <annaphillips416@gmail.com>
2023-07-30 16:49:34 -0400

    nav menu styling

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
@@ -10,6 +10,7 @@ import './Navigation.css';
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
+  const [showNav, setShowNav] = useState(false);
 
   const dispatch = useDispatch()
   const handleDemo = (e) => {
@@ -32,39 +33,56 @@ function Navigation({ isLoaded }) {
   } else {
     sessionLinks = (
       <>
-        <li>
-          <OpenModalButton
-            buttonText="Log In"
-            modalComponent={<LoginFormModal />}
-          /></li>
-        <li>
+        <li className='nav-button'>
           <OpenModalButton
             buttonText="Sign Up"
             modalComponent={<SignupFormModal />}
           />
         </li>
+        <li className='nav-button'>
+          <OpenModalButton
+            buttonText="Log In"
+            modalComponent={<LoginFormModal />}
+          /></li>
       </>
     );
   }
 
+  const toggleShowNav = () => {
+    setShowNav(!showNav)
+  }
+
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      const navContainer = document.querySelector('.nav-right-container');
+      if (navContainer && !navContainer.contains(e.target)) {
+        setShowNav(false);
+      }
+    };
+
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
   return (
-    // <div className='navBar'>
-    //   <NavLink exact to="/" className='home'><img src='https://res.cloudinary.com/duakjbyfi/image/upload/v1678545268/AirBnB%20Clone/logo_browser.psd_vhyhya.png' /></NavLink>
-    //   <ul className='nav'><i class="fa-solid fa-bars"></i>
-    //     {isLoaded && sessionLinks}
-    //     {!sessionUser
-    //     ? <li className="nav-button"><button onClick={handleDemo}>Demo Login</button></li>
-    //     : <></>}
-    //   </ul>
-    // </div>
     <div className='navbar'>
       <NavLink exact to="/" className='home'><img src='https://res.cloudinary.com/duakjbyfi/image/upload/v1678545268/AirBnB%20Clone/logo_browser.psd_vhyhya.png' /></NavLink>
       <div className='nav-right-container'>
-        <div className='your-home'>Airbnb your home</div>
-        <div className='user-icon'>
-        <i class="fa-solid fa-bars" style={{ fontSize: '15px' }}></i>
+        <NavLink exact to="/spots/new" className='your-home'>Airbnb your home</NavLink>
+        <div className='user-icon' onClick={toggleShowNav}>
+          <i class="fa-solid fa-bars" style={{ fontSize: '15px' }} />
           <i class="fa-solid fa-circle-user" />
         </div>
+        {showNav &&
+        <ul className='nav'>
+        {isLoaded && sessionLinks}
+        {!sessionUser
+        ? <li className="nav-button"><button onClick={handleDemo}>Demo Login</button></li>
+        : <></>}
+      </ul>}
       </div>
     </div>
   );
